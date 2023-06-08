99131b79a98a5b1b901306dbcb39b40ebfb5f6a3
Eva Ramos <eva.ramos@iesnervion.es>
2023-06-08 12:16:04 +0200

    1

---
+++ ahorcado/Entidades/clsPalabra.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class clsPalabra
+    {
+        #region Propiedades
+
+        public int id { get; set; }
+
+        public string nombre { get; set; }
+
+
+        #endregion
+
+        #region Constructores
+        public clsPalabra()
+        {
+
+        }
+        public clsPalabra(int id,string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        #endregion
+       
+    }
+}
