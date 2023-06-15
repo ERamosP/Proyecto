19023d88d7497376e6dc0734f3f5b0e9b113afbc
DiegoHurtado2003 <93648633+DiegoHurtado2003@users.noreply.github.com>
2023-06-15 11:00:34 +0200

    Método api 5 palabras

---
+++ ahorcado/proyectoFinDeGradoAhorcado/Controllers/api5palabras.cs
@@ -0,0 +1,27 @@
+﻿using Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace proyectoFinDeGradoAhorcado.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class api5palabras : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                List<clsPalabra> ListadoPalabras = DAL.Manejadora.ManejadoraPalabras_DAL.get5PalabrasRandom();
+                return Ok(ListadoPalabras);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
